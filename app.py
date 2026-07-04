9247fa0fc7f12b74cebad78ca3579da6b94ba1c2
Preston Werner <prestoncwerner@gmail.com>
2026-07-04 16:10:03 -0500

    Changed validation for API Auth Key form submission and Player Name/Game Count form submission

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ import streamlit as st
 @st.dialog("Ballchasing API Auth Key Submission", dismissible = False)
 def set_api_auth_key() -> None:
     with st.form(key="api_key_submission", clear_on_submit=True):
-        api_auth_key: str = st.text_input(label = "**ENTER AUTH KEY**", value="", placeholder = "ABCDEFGHI12345678", type = "password", max_chars = 40, width = 200)
+        api_auth_key: str = st.text_input(label = "**ENTER AUTH KEY**", value=None, placeholder = "ABCDEFGHI12345678", type = "password", max_chars = 40, width = 200)
         submit_button = st.form_submit_button(label="Submit")
 
         if submit_button:
-            st.session_state["api_key"] = api_auth_key
-            st.success("API Authorization Key added to this session. You may close this window.")
-            st.rerun()
+            if api_auth_key is None:
+                st.error("Please put an API Auth Key in the text input.")
+            elif not api_auth_key.isalnum():
+                st.error("API Auth Key is not a valid key. Please double-check your Ballchasing API key.")
+            elif len(api_auth_key) < 40:
+                st.error(f"API Auth Key is not a valid key. Auth Key was of length {len(api_auth_key)} characters, but expected 40 characters. Please double-check your Ballchasing API key.")
+            else:
+                st.session_state["api_key"] = api_auth_key
+                st.success("API Authorization Key added to this session. You may close this window.")
+                st.rerun()
 
 
 home_page = st.Page("views/pull_data.py", title="Home - Ingest Data", icon="🏠", default=True)
--- views/pull_data.py
+++ views/pull_data.py
@@ -2,12 +2,33 @@ import streamlit as st
 import pandas as pd
 from utils.player_replay_ingestion import ingest_data
 
+@st.dialog("Ballchasing API Auth Key Submission", dismissible = False)
+def set_api_auth_key() -> None:
+    with st.form(key="api_key_submission", clear_on_submit=True):
+        api_auth_key: str = st.text_input(label = "**ENTER AUTH KEY**", value=None, placeholder = "ABCDEFGHI12345678", type = "password", max_chars = 40, width = 200)
+        submit_button = st.form_submit_button(label="Submit")
+
+        if submit_button:
+            if api_auth_key is None:
+                st.error("Please put an API Auth Key in the text input.")
+            elif not api_auth_key.isalnum():
+                st.error("API Auth Key is not a valid key. Please double-check your Ballchasing API key.")
+            elif len(api_auth_key) < 40:
+                st.error(f"API Auth Key is not a valid key. Auth Key was of length {len(api_auth_key)} characters, but expected 40 characters. Please double-check your Ballchasing API key.")
+            else:
+                st.session_state["api_key"] = api_auth_key
+                st.success("API Authorization Key added to this session. You may close this window.")
+                st.rerun()
 
 if __name__ == "__main__":
     st.set_page_config(layout = "centered")
     with st.sidebar:
         if st.button("Clear Cache"):
             st.cache_data.clear()
+        
+        if st.button("Change API Authentication Key"):
+            set_api_auth_key()
+
     with st.container():
         st.title("Ballchasing Data Ingestion", text_alignment = "center")
         st.space(size = "large")
@@ -18,26 +39,16 @@ if __name__ == "__main__":
 
             submit_button = st.form_submit_button(label="Submit")
 
-        name_validation_list: list = [" ", "/", "\\", "<", ">", "\"", "\'"]
-        char_flag = False
-        df_exists = False
-
         if submit_button:
-            for char in player_name:
-                if char in name_validation_list:
-                    st.error(f"Character '{char}' is not a valid character in '{player_name}'.")
-                    char_flag = True
-                    break
             
-            if game_count is None:
+            if not player_name.isalnum():
+                st.error(f"'{player_name}' must be comprised of alpha-numeric characters.")
+            elif game_count is None:
                 st.error("Game count must not be null.")
-                
-            
-            if player_name + "_" + str(game_count) in st.session_state["df_manifest"]:
+            elif player_name + "_" + str(game_count) in st.session_state["df_manifest"]:
                 st.error(f"DataFrame already exists for player {player_name}'s last {game_count} games.")
-                df_exists = True
-            
-            if not char_flag and game_count is not None and not df_exists:
+                df_exists = True         
+            else:
                 st.success(f"Form submitted successfully. Pulling {player_name}'s Ballchasing data from the last {game_count} games!")
                 with st.spinner("Loading data..."):
                     new_df: pd.DataFrame = ingest_data(player_name, game_count, st.session_state["api_key"])
